d3b7e3b6b3ad44753bf6cfab84105d36b9ccda04
ahmedfou <ahmedfou2014@gmail.com>
2020-06-14 14:12:55 +0100

    add more impls for jwt validation and verifaction and cretion

---
+++ jwt/src/main/java/com/foughali/jwt/domain/User.java
@@ -0,0 +1,34 @@
+package com.foughali.jwt.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String username;
+
+    @Column
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ jwt/src/main/java/com/foughali/jwt/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.foughali.jwt.repository;
+
+
+import com.foughali.jwt.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUsername(String username);
+}
